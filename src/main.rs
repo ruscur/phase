74908ab3d12ca446316401ea6f3e85007a8b3de5
Russell Currey <ruscur@russell.cc>
2016-02-12 19:32:02 +1100

    Display an image! WOO
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+extern crate sdl2;
+extern crate sdl2_image;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2_image::{LoadTexture};
+
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let screen_width = 800;
+    let screen_height = 600;
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("phase", screen_width, screen_height)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut renderer = window.renderer().build().unwrap();
+    let texture = renderer.load_texture(&Path::new("/home/ruscur/Documents/phase/assets/png/hello.png")).unwrap();
+
+    renderer.set_draw_color(Color::RGB(255, 255, 255));
+    renderer.clear();
+    renderer.copy(&texture, None, None);
+    renderer.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running : loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+    }
 }
