ca0b909d7b35123805351369a92c0ac1f950b77d
Russell Currey <ruscur@russell.cc>
2016-02-12 20:46:06 +1100

    Refactor crappy style and scale the image to a hardcoded size
    This is in line with tutorial 05 on lazyfoo.net.  It turns out that
    Rust's renderer.copy() - which is the equivalent of "blit" - defaults
    to using the entire space rather than the original size of the texture.
    So, rather than make a new rect to scale it up, we're making a new rect
    to scale it down.
    
    I'm going to try to document differences I notice between how things
    work in C and how I think they should be translated into Rust.  Maybe
    once I'm further in I can write a blog post about it or something.
    
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,36 @@ extern crate sdl2_image;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2_image::{LoadTexture};
+use sdl2::rect::Rect;
+use sdl2_image::LoadTexture;
 
 use std::path::Path;
 
-fn main() {
-    let screen_width = 800;
-    let screen_height = 600;
+const SCREEN_WIDTH: u32 = 800;
+const SCREEN_HEIGHT: u32 = 600;
 
+fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem.window("phase", screen_width, screen_height)
+    let window = video_subsystem.window("phase", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
         .opengl()
         .build()
-        .unwrap();
+        .unwrap_or_else(|err| panic!("Couldn't make a window: {}", err));
 
     let mut renderer = window.renderer().build().unwrap();
-    let texture = renderer.load_texture(&Path::new("/home/ruscur/Documents/phase/assets/png/hello.png")).unwrap();
+    let texture = renderer.load_texture(
+        &Path::new("/home/ruscur/Documents/phase/assets/png/hello.png"))
+        .unwrap_or_else(|err| panic!("Failed to open texture: {}", err));
 
     renderer.set_draw_color(Color::RGB(255, 255, 255));
     renderer.clear();
-    renderer.copy(&texture, None, None);
+    let rect = Rect::new(0, 0, SCREEN_WIDTH - 300, SCREEN_HEIGHT - 200).unwrap();
+    renderer.copy(&texture, None, rect);
     renderer.present();
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut event_pump = sdl_context.event_pump()
+        .unwrap_or_else(|err| panic!("Couldn't get event pump: {}", err));
 
     'running : loop {
         for event in event_pump.poll_iter() {
