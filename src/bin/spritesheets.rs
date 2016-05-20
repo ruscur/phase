4bf411592dc9604fa61b77c02315948ade4f6f0e
Russell Currey <ruscur@russell.cc>
2016-05-20 22:12:50 +1000

    Vec<Rect> whitespace fixes
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- src/bin/spritesheets.rs
+++ src/bin/spritesheets.rs
@@ -29,17 +29,18 @@ fn main() {
     let mut event_pump = sdl_context.event_pump()
         .unwrap_or_else(|err| panic!("Couldn't get event pump: {}", err));
 
-    let dots: Vec<Rect> = vec!(Rect::new(0, 0, 100, 100),
-                            Rect::new(100, 0, 100, 100),
-                            Rect::new(0, 100, 100, 100),
-                            Rect::new(100, 100, 100, 100));
+    let dots: Vec<Rect> = vec!(
+        Rect::new(0, 0, 100, 100),
+        Rect::new(100, 0, 100, 100),
+        Rect::new(0, 100, 100, 100),
+        Rect::new(100, 100, 100, 100));
 
-    let dests: Vec<Rect> = vec!(Rect::new(0, 0, 100, 100),
-                            Rect::new((SCREEN_WIDTH - 100) as i32, 0, 100, 100),
-                            Rect::new(0, (SCREEN_HEIGHT - 100) as i32,
-                                100, 100),
-                            Rect::new((SCREEN_WIDTH - 100) as i32,
-                                (SCREEN_HEIGHT - 100) as i32, 100, 100));
+    let dests: Vec<Rect> = vec!(
+        Rect::new(0, 0, 100, 100),
+        Rect::new((SCREEN_WIDTH - 100) as i32, 0, 100, 100),
+        Rect::new(0, (SCREEN_HEIGHT - 100) as i32, 100, 100),
+        Rect::new((SCREEN_WIDTH - 100) as i32, (SCREEN_HEIGHT - 100) as i32,
+            100, 100));
 
     'running : loop {
         for event in event_pump.poll_iter() {
